3b7d76c35c6b40dbafec775b0c8e47e2cfad0012
SamuelFriasNieto <sae.nieto@gmail.com>
2024-01-28 15:43:57 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+
+      <header>
+        <form action="">
+          <input type="text" />
+          <button>Buscar</button>
+        </form>
+      </header>
+
+    </div>
+  )
+}
+
+export default App
